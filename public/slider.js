e3f7f12fefd53cb980dbb66f62cf4e75b7edcb05
Roger <thrasher_royerr666@hotmail.com>
2019-12-31 11:06:16 -0600

    first commit add search bot

--- public/slider.js
+++ public/slider.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    $(".bxslider").bxSlider({
-      mode: "fade",
-      captions: true,
-      slideWidth: 600
-    });
+$(document).ready(function () {
+  $(".bxslider").bxSlider({
+    mode: "fade",
+    captions: true,
+    slideWidth: 865,
   });
+});
