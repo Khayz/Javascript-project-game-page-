91b0fb65bead387a221d8d4c87347aa185833bdd
Khayz <altaircarn@gmail.com>
2020-01-03 01:42:41 -0600

    gggg

---
+++ public/searchGames.js
@@ -0,0 +1,34 @@
+const searchGame = document.querySelector('.searchGame')
+const searchedGames = document.querySelector('.searchedGames')
+
+const eventSearchGame = (data) => {
+  searchGame.addEventListener('keyup', (event) => {
+    const gameArray = []
+    data.map(e => {
+      const inputValue = event.target.value
+      const nameOfGame = e.name
+      const regExpName = RegExp(inputValue, 'gi')
+      const findGame = regExpName.test(nameOfGame)
+      if (findGame && inputValue) {
+        gameArray.push(e.name)
+      }
+    })
+    const newGameArray = [...new Set(gameArray)]
+    const innerToDiv = []
+    newGameArray.map(e => innerToDiv.push(`<p>${e}</p>`))
+    searchedGames.innerHTML = innerToDiv.join('')
+  })
+}
+
+
+const getData = async () => {
+  const resolveData = await fetch('https://api.rawg.io/api/games?page=1')
+  const data = await resolveData.json()
+  console.log(data.results)
+  eventSearchGame(data.results)
+}
+
+
+(() => {
+  getData()
+})()
